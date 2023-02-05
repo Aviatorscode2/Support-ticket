179018018d4048e17efbc8f6e2f075c1156276fa
Aviatorscode2 <victoryakubu5@gmail.com>
2023-02-06 00:42:02 +0100

    error handling using asyncHandler

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,29 +1,30 @@
+const asyncHandler = require('express-async-handler');
 // Description: This file contains all the functions that are used to handle the user routes
 // @desc Register a new user
 // @route POST /api/users
 // @access Public
 
-const registerUser = (req, res) => {
-    const {name, email, password} = (req.body);
+const registerUser = asyncHandler (async(req, res) => {
+  const { name, email, password } = req.body;
 
-    // Simple validation
-    if(!name || !email || !password) {
-        res.status(400);
-        throw new Error('Please enter all fields');
-    }
+  // Simple validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please enter all fields');
+  }
 
-    res.send('Register Route')
- };
+  res.send('Register Route');
+});
 
- // @desc Login a user
+// @desc Login a user
 // @route POST /api/users/login
 // @access Public
 
-const loginUser = (req, res) => {
-    res.send('Login Route');
- }
+const loginUser = asyncHandler(async (req, res) => {
+  res.send('Login Route');
+});
 
 module.exports = {
-    registerUser,
-    loginUser
+  registerUser,
+  loginUser,
 };
