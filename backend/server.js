9b940a01c78320af2b80fb4887773d6175cd44bf
Aviatorscode2 <victoryakubu5@gmail.com>
2023-02-06 00:21:12 +0100

    Add controllers and routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("...Server ");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+
+// Create Express server
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Ticket API' });
+});
+
+// routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
