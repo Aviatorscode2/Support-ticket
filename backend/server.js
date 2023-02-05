4085fda71dcd8eb369acad97d5117b7375583562
Aviatorscode2 <victoryakubu5@gmail.com>
2023-02-06 00:51:27 +0100

    connected to the MongDB database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const {errorHanddler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
 
+// Connect to database
+connectDB();
 // Create Express server
 const app = express();
 
