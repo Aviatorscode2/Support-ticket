353e4939be17fad075009dd627b1d50637091eef
Aviatorscode2 <victoryakubu5@gmail.com>
2023-02-06 01:13:32 +0100

    We are now able to reqister a user

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/userModel');
 // Description: This file contains all the functions that are used to handle the user routes
 // @desc Register a new user
 // @route POST /api/users
@@ -13,7 +16,35 @@ const registerUser = asyncHandler (async(req, res) => {
     throw new Error('Please enter all fields');
   }
 
-  res.send('Register Route');
+  // Check for existing user
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  // Create salt & hash
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  // Create new user
+  const user = await User.create({
+    name,
+    email,
+    password: hash,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // @desc Login a user
