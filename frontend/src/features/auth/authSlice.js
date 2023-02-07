d5de808571085e8cffbd48877187cabfe9e7d932
Aviatorscode2 <victoryakubu5@gmail.com>
2023-02-07 21:33:30 +0100

    hooked the login for to redux

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -9,7 +9,13 @@ const initialState = {
     message: ''
 };
 
+// register new user
 export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
+    console.log(user);
+});
+
+// login user
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
     console.log(user);
 });
 export const authSlice = createSlice({
