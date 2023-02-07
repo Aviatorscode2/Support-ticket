a1ea09635b9f08ea8e668286615d316304fb61b5
Aviatorscode2 <victoryakubu5@gmail.com>
2023-02-07 21:28:55 +0100

    Hoooking register form to redux

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -9,6 +9,9 @@ const initialState = {
     message: ''
 };
 
+export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
+    console.log(user);
+});
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
