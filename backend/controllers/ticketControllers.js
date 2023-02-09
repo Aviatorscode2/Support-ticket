3bfb1f44344769f4e5daaf5cfe1559cb09c12e77
Aviatorscode2 <victoryakubu5@gmail.com>
2023-02-09 13:15:03 +0100

    get tickets from backend

--- backend/controllers/ticketControllers.js
+++ backend/controllers/ticketControllers.js
@@ -8,7 +8,18 @@ const Ticket = require('../models/ticketModel');
 // @access  Private
 
 const getTickets = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'getTickets'});
+
+    // get user using the ID in the JWT
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        res.status(404);
+        throw new Error('User not found');
+    }
+
+    // get tickets for the user
+    const tickets = await Ticket.find({user: user._id});
+    res.status(200).json(tickets);
 });
 
 // @desc    Create a ticket
