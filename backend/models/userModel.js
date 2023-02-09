226c3fda5701c2621df878f48088c902b46e87eb
Aviatorscode2 <victoryakubu5@gmail.com>
2023-02-09 15:43:03 +0100

    functionality for getting a single ticket

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -8,6 +8,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, 'Please add an email'],
+        unique: true,
     },
     password: {
         type: String,
