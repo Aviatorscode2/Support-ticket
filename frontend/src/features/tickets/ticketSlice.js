3abee0dc0649f695f1ebdecbafa6418ee6dcf46a
Aviatorscode2 <victoryakubu5@gmail.com>
2023-02-10 01:32:24 +0100

    Added Tickets to redux

---
+++ frontend/src/features/tickets/ticketSlice.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import ticketService from './ticketService'
+
+const initialState = {
+    tickets: [],
+    ticket: {},
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: '',
+}
+
+export const ticketSlice = createSlice({
+    name: 'ticket',
+    initialState,
+    reducers: {
+        reset: (state) => initialState
+    },
+    extraReducers: (builder) => {}
+});
+
+export const {reset} = ticketSlice.actions;
+export default ticketSlice.reducer;
