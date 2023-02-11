a367a403e4a31e3854d21ea5cd8cda0344822890
Aviatorscode2 <victoryakubu5@gmail.com>
2023-02-11 22:50:01 +0100

    Added a back button to the new ticket page

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import {toast} from 'react-toastify';
 import { createTicket, reset } from "../features/tickets/ticketSlice";
 import Spinner from "../components/Spinner";
+import BackButton from '../components/BackButton';
 
 function NewTicket() {
     const {user} = useSelector(state => state.auth);
@@ -41,6 +42,7 @@ function NewTicket() {
 
     return (
       <>
+        <BackButton url='/'/>
         <section className="heading">
           <h1>Create a new ticket</h1>
           <p>Please fill out the form below to create a new ticket</p>
